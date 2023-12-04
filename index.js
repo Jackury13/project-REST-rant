106b4fbf91c14edd55be0a3a862eeb4ae925e1a3
Jackury13 <143567012+Jackury13@users.noreply.github.com>
2023-12-04 02:04:32 -0800

    new places can be added!!

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ app.get('/', (req, res) => {
     res.render(`home`)
 })
 
-
 app.get('*', (req, res) => {
     res.render ('error404')
 })
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: "/images/dan-gold-E6HjQaB7UEA-unsplash.jpg"
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: "/images/nathan-dumlao-zUNs99PGDg0-unsplash.jpg"
+  }]
